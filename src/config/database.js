29b476195a4ab9f0ca00797c8961a61d14319919
Thiago Souza <75909647+ThiagoEdSouza@users.noreply.github.com>
2024-04-25 20:36:55 -0300

    Update database.js - add url banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 module.exports = {
     dialect: 'postgres', //Indicando qua o banco de dados utilizado
-    host: 'localhost', //Indicando o endereço do banco.
-    username: 'postgres', //Indicando o usuário do banco
-    password: 'postgres', //Indicando a senha do banco
-    database: 'codeburger', //Indicando o nome do bando de dados conforme criado no Postbird
+    url:'postgresql://postgres:RdSagjCQtCkIdPNEcVhlxhudOSUtyJTA@viaduct.proxy.rlwy.net:39621/railway',
     define: { //Criando algumas definições
         timestamps: true, //Auxilia na rastreabilidade dos nossos dados por criar as datas de criação e alteração dos dados
         underscored: true,
